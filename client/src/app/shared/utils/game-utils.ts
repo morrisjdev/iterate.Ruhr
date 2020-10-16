47631c94b9e4c0a71b1548e8e5eecf1ca8e3a56f
Morris Janatzek <mjanatzek@brockhaus-ag.de>
2020-10-16 08:21:40 +0200

    fixed game utils get winner method

--- client/src/app/shared/utils/game-utils.ts
+++ client/src/app/shared/utils/game-utils.ts
@@ -1,6 +1,11 @@
 import {Field} from '../models/field';
 
 export class GameUtils {
+  /**
+   * Calculates the winner of the provided fields
+   * @returns The number of the winner or -1 if no winner exists
+   * @param fields The fields of a game
+   */
   public static getWinner(fields: Field[]): number {
     const matrix: Field[][] = [[], [], []];
 
@@ -8,21 +13,21 @@ export class GameUtils {
       matrix[field.x][field.y] = field;
     }
 
-    // Zeilen prüfen
+    // Check rows
     for (let x = 0; x <= 2; x++) {
       if (matrix[x][0].value !== 0 && matrix[x][0].value === matrix[x][1].value && matrix[x][1].value === matrix[x][2].value) {
         return matrix[x][0].value;
       }
     }
 
-    // Spalten prüfen
+    // Check columns
     for (let y = 0; y <= 2; y++) {
-      if (matrix[0][y].value !== 0 && matrix[0][y].value === matrix[1][y].value && matrix[1][y].value === matrix[1][y].value) {
+      if (matrix[0][y].value !== 0 && matrix[0][y].value === matrix[1][y].value && matrix[1][y].value === matrix[2][y].value) {
         return matrix[0][y].value;
       }
     }
 
-    // Diagonal prüfen
+    // Check diagonals
     if (matrix[0][0].value !== 0 && matrix[0][0].value === matrix[1][1].value && matrix[1][1].value === matrix[2][2].value) {
       return matrix[0][0].value;
     }
